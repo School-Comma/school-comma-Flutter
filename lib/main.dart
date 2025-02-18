b3e8d392679d552fd365f724db6fd7b430e0aba3
taejun <ptj01036251@gmail.com>
2025-02-18 10:51:35 +0900

    feat: 로그인 화면 퍼블리싱

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:school_comma/presentaion/user/login_screen.dart';
 
 void main() {
   runApp(const Main());
@@ -12,7 +13,10 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(430, 942),
-      child: MaterialApp(),
-    )
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen(),
+      ),
+    );
   }
 }
